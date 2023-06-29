04dd1731dc02ed07a74361b9b002e965c3459664
NiuBoss123 <i@niuboss123.com>
2023-06-29 18:29:45 +0800

    2023-06-29-01

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -4,12 +4,16 @@ import { defineConfig } from 'vitepress'
 export default defineConfig({
   title: "音留文档",
   description: "音留网络官方文档",
+  base: '/docs/',
+  lang: 'zh-cn',
+  srcDir: './index',
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
     nav: [
-      { text: '主页', link: '/' },
+      { text: '文档主页', link: '/' },
       { text: '音留官网', link: 'https://www.holdvoice.cn' },
       { text: '壹阿尔', link: 'https://www.yiair.cc' },
+      { text: '音留社区', link: 'https://club.holdvoice.cn' },
       { text: '堂瞰工艺', link: 'https://tc.holdvoice.cn' },
     ],
 
@@ -17,15 +21,15 @@ export default defineConfig({
       {
         text: 'HoldVoice 音留网络',
         items: [
-          { text: '账号系统', link: '/account' },
-          { text: '联系方式', link: '/' },
+          { text: '账号系统', link: '/holdvoice/account' },
+          { text: '联系方式', link: '/contact#音留网络统括理事会' },
         ]
       },
       {
         text: 'Yiair 壹阿尔',
         items: [
           { text: '基本服务协议', link: '/yiair' },
-          { text: '联系方式', link: '/#yiair' },
+          { text: '联系方式', link: '/contact#壹阿尔团队' },
         ]
       },
       {
@@ -34,14 +38,14 @@ export default defineConfig({
           { text: '用户协议', link: '/club/terms' },
           { text: '社区规范', link: '/club/protocol' },
           { text: '隐私政策', link: '/club/privacy' },
-          { text: '联系方式', link: '/#club' },
+          { text: '联系方式', link: '/contact#音留社区管理组' },
         ]
       },
       {
         text: 'TangibleCraft 堂瞰社区',
         items: [
           { text: '玩家条款', link: '/tangiblecraft' },
-          { text: '联系方式', link: '/#tangiblecraft' },
+          { text: '联系方式', link: '/contact#堂瞰工艺风纪委员' },
         ]
       },
     ],
---
+++ index/contact.md
@@ -0,0 +1,32 @@
+# 联系我们
+
+## 音留网络统括理事会
+
+#### 统括理事长：御坂秋生（Misaka Akio）
+- 邮箱：[i@niuboss123.com](mailto:i@niuboss123.com)
+- Telegram：[@NiuBoss123](https://t.me/NiuBoss123)
+
+## 壹阿尔团队
+
+#### 队长：御坂秋生（Misaka Akio）
+- 邮箱：[i@niuboss123.com](mailto:i@niuboss123.com)
+- Telegram：[@NiuBoss123](https://t.me/NiuBoss123)
+
+## 音留社区管理组
+
+#### 管理组组长：御坂（Misaka）
+- 邮箱：[bilibili0413@qq.com](mailto:bilibili0413@qq.com)
+
+#### 管理组组员：御坂秋生（Misaka Akio）
+- 邮箱：[i@niuboss123.com](mailto:i@niuboss123.com)
+- Telegram：[@NiuBoss123](https://t.me/NiuBoss123)
+
+## 堂瞰工艺风纪委员
+::: tip
+确保你需要直接与风纪委员长直接对话，否则我们建议您优先联系其他风纪委员，避免给风纪委员长造成困扰
+:::
+
+#### 风纪委员长：NiuBoss123
+- 邮箱：[i@niuboss123.com](mailto:i@niuboss123.com)
+- Telegram：[@NiuBoss123](https://t.me/NiuBoss123)
+
